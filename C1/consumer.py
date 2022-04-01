14a5fc4b6634823014d05dab56c3cdca5fae4077
lovvvan <lovisa.hedman93@gmail.com>
2022-04-01 11:30:44 +0000

    consumer prints words in while true loop, not good solution

--- C1/consumer.py
+++ C1/consumer.py
@@ -3,24 +3,29 @@ from conversion import conversion
 from conversion import function
 
 # Create a pulsar client by supplying ip address and port 
-client = pulsar.Client('pulsar://localhost:6650') 
-  
-# Subscribe to a topic and subscription  
-consumer = client.subscribe('DEtopic', subscription_name='DE-sub') 
+def pulsarConsumer():
+  # Create a pulsar client by supplying ip address and port
+  client = pulsar.Client('pulsar://localhost:6650') 
+  # Subscribe to a topic and subscription  
+  return client.subscribe('DEtopic', subscription_name='DE-sub') 
    
-# Display message received from producer 
-msg = consumer.receive() 
-    
-try: 
-  print("Received message : '%s'" % msg.data()) 
+consumer = pulsarConsumer() 
+merged_string = ""
 
-  upper_case_string = conversion(msg.data(), function)
-  print("upper case string: ", upper_case_string)
- 
-  # Acknowledge for receiving the message  
-  consumer.acknowledge(msg) 
-                  
-except: 
-  consumer.negative_acknowledge(msg) 
+while (True):
+    try: 
+        # Display message received from producer
+        msg = consumer.receive()
+        print("Received message : '%s'" % msg.data()) 
+
+        upper_case_string = conversion(msg.data(), function)
+        print("upper case string: ", upper_case_string)
+        merged_string += upper_case_string
+        # Acknowledge for receiving the message  
+        consumer.acknowledge(msg) 
+    except: 
+        consumer.negative_acknowledge(msg) 
+
+print(merged_string)
 # Destroy pulsar client 
 client.close() 
