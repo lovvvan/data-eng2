69aad4fbfc623b59f18ad699700ba4474861862a
lovvvan <lovisa.hedman93@gmail.com>
2022-05-08 15:25:01 +0200

    changed cloud init

--- ssc-instance-C3.py
+++ ssc-instance-C3.py
@@ -7,7 +7,7 @@ import keystoneclient.v3.client as ksclient
 from keystoneauth1 import loading
 from keystoneauth1 import session
 
-flavor = "ssc.medium"
+flavor = "ssc.small"
 private_net = "UPPMAX 2022/1-1 Internal IPv4 Network"
 floating_ip_pool_name = "Public External IPv4 Network"
 image_name = "Ubuntu 20.04 - 2021.03.23"
@@ -43,10 +43,10 @@ if os.path.isfile(cfg_file_path):
 else:
     sys.exit("cloud-cfg.txt is not in current working directory")
 
-secgroups = ['default']
+secgroups = ['default', 'lovisa-hedman']
 
 print ("Creating instance ... ")
-instance = nova.servers.create(name="Lovisa-hedman-C3", key_name="lovisa-key-data2", image=image, flavor=flavor, userdata=userdata, nics=nics,security_groups=secgroups)
+instance = nova.servers.create(name="Lovisa-hedman-C3-1", key_name="lovisa-key-data2", image=image, flavor=flavor, userdata=userdata, nics=nics,security_groups=secgroups)
 inst_status = instance.status
 print ("waiting for 10 seconds.. ")
 time.sleep(10)
